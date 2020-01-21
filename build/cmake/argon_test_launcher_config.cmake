c66ba08f6968846ed3fceef291c2c02f27b0564f
lagobas <maxim.kolesnik90@gmail.com>
2020-01-22 00:43:22 +0300

    Update travis

--- build/cmake/argon_test_launcher_config.cmake
+++ build/cmake/argon_test_launcher_config.cmake
@@ -1,12 +1,12 @@
-cmake_minimum_required (VERSION 3.13.3)
+cmake_minimum_required (VERSION 3.16.2)
 
 project (argon_unit_testing)
 
-set (ARGON_PROJECTS
-	fundamental
-	data_structures
-	math
-	third_party/gtest
-	unit_testing/data_structures_test
-	unit_testing/test_launcher
-)
+add_subdirectory(fundamental)
+add_subdirectory(data_structures)
+add_subdirectory(math)
+add_subdirectory(third_party/gl)
+add_subdirectory(third_party/glfw)
+add_subdirectory(third_party/gtest)
+add_subdirectory(unit_testing/data_structures_test)
+add_subdirectory(unit_testing/test_launcher)
